64dbc05dde28b05d64121d0a4380644f2e544f18
Vivaswan Shukla <107896809+VivaswanShukla@users.noreply.github.com>
2024-03-30 13:23:39 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,14 @@
 //This is a repository
+public class AddNumbers {
+  //VivaswanShukla
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = add(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
