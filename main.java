a1bcb1e8088382a3e1033e833a2447f50bdfd8bd
Sidd569 <145440509+Sidd569@users.noreply.github.com>
2024-03-30 13:47:58 +0530

    Update main.java

--- main.java
+++ main.java
@@ -179,3 +179,19 @@ class add{
 
     }
 }
+//***********************************************************************
+//Siddhant Bhatnagar
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
