0db69c4a6f1bb61ba93e0283a73252851c4e9074
Syed Safi <98023624+syedgithub12345@users.noreply.github.com>
2024-03-30 13:32:52 +0530

    Update main.java

--- main.java
+++ main.java
@@ -35,3 +35,59 @@ public class binarySearchUnsort{
 
 	}
 }
+
+//********************************************************************
+//Syed Safi Work
+public class Numbers {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = add(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        int subt=sub(int num1,int num2);
+        System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + subt);
+
+        int mult=multi(int num1,int num2);
+        System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + mult);
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a,int b){
+        return a-b;
+    }
+  
+    public static int multi(int a, int b)
+    {
+        return a*b;
+    }
+}
+//***********************************************************************
+//Syed Safi Work
+//insertion sort
+public class insertion {
+    public static void main(String[] args) {
+        int[] arr = { 3, 8, 90, 12, 1, 76, 9 };
+        int n = arr.length;
+        System.out.println("Before insertion sort :");
+        for (int l : arr) {
+            System.out.print(l + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println("After sorting :");
+        for (int m : arr) {
+            System.out.print(m + " ");
+        }
+    }
+}
+//**********************************************************************
