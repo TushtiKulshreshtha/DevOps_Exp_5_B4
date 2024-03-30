3a19a8f5a4c435b6afdff7e356601fb9f889716e
Tushti Kulshreshtha <103263767+TushtiKulshreshtha@users.noreply.github.com>
2024-03-30 13:35:53 +0530

    Update main.java

--- main.java
+++ main.java
@@ -91,3 +91,43 @@ public class insertion {
     }
 }
 //**********************************************************************
+
+//-------------------------------------------------------------------------------------
+
+//Tushti Kulshreshtha
+//Quick Sort
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] array = {9, 2, 4, 7, 3, 7, 10};
+        quickSort(array, 0, array.length - 1);
+        System.out.println("Sorted array: " + Arrays.toString(array));
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(array, low, high);
+            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+}
+
+//--------------------------------------------------------------------------------------------
