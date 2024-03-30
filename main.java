a2ff88b59713dc79a24e18eb2e439e36b4510618
Bhoomitiwari <151936313+Bhoomitiwari@users.noreply.github.com>
2024-03-30 13:50:07 +0530

    Update main.java

--- main.java
+++ main.java
@@ -195,3 +195,29 @@ public class BubbleSort {
                 }
             }
         }
+// **********************************************************************
+//Bhoomi's Work
+public class RectanglePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows:");
+        int row = scanner.nextInt();
+        
+        System.out.println("Enter the number of columns:");
+        int col = scanner.nextInt();
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || i == (row - 1) || j == 0 || j == (col - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
